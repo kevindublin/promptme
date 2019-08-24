2292f96bae121dd77dc3907798d809fbaeb28909
Vin Dublin <kevdublin@gmail.com>
2019-08-23 18:32:38 -0700

    TinyMCE integrated

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from tinymce.widgets import TinyMCE
 from django import forms
+from tinymce.models import HTMLField
 
 
-class Form(forms.ModelForm):
+class PostForm(forms.ModelForm):
     ...
     content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
+    ...
+
+
+class WriteBox(models.Model):
+    ...
+    content = HTMLField()
     ...

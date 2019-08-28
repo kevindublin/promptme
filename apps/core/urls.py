976a15fe4db54436abc81b64b2857040f1558d7d
Vin Dublin <kevdublin@gmail.com>
2019-08-28 02:43:20 -0700

    placeholder pages and templates

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,6 +4,12 @@ from apps.core import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('about/', views.about, name='about'),
     path('form', views.form, name='form'),
+    path('feedbackq/', views.feedbackq, name='feedbackq'),
+    path('prompt/', views.prompt, name='prompt'),
+    path('write/', views.write, name='write'),
+    path('edit/', views.edit, name='edit'),
+    path('membership', views.membership, name='membership'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -10,6 +10,13 @@ def home(request):
     return render(request, 'pages/home.html', context)
 
 
+def dashboard(request):
+    context = {
+    }
+
+    return render(request, 'pages/dashboard.html', context)
+
+
 def about(request):
     context = {
     }
@@ -17,8 +24,43 @@ def about(request):
     return render(request, 'pages/about.html', context)
 
 
+def membership(request):
+    context = {
+    }
+
+    return render(request, 'pages/membership.html', context)
+
+
 def form(request):
     context = {
     }
 
     return render(request, 'pages/form.html', context)
+
+
+def feedbackq(request):
+    context = {
+    }
+
+    return render(request, 'pages/feedbackq.html', context)
+
+
+def prompt(request):
+    context = {
+    }
+
+    return render(request, 'pages/prompt.html', context)
+
+
+def write(request):
+    context = {
+    }
+
+    return render(request, 'pages/write.html', context)
+
+
+def edit(request):
+    context = {
+    }
+
+    return render(request, 'pages/edit.html', context)

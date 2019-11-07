a2c269f0a92f3b00002de6d396fa90e32b1b992d
Kevin Dublin <kevin@kevindublin.com>
2019-11-07 09:36:37 -0800

    line break fix for tinyMCE

--- apps/core/static/js/tinymce/custom.js
+++ apps/core/static/js/tinymce/custom.js
@@ -14,8 +14,6 @@ tinymce.init({
     'insertdatetime media paste code wordcount'
   ],
   toolbar: 'undo redo | formatselect | bold italic backcolor | bullist numlist outdent indent | removeformat',
-  force_p_newlines : true,
-  force_br_newlines : true,
-  remove_linebreaks : false,
+
 });
 

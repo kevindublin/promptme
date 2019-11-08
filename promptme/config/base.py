9efc40a72e3570d52487f6703e05f381374112f8
Kevin Dublin <kevin@kevindublin.com>
2019-11-08 14:25:02 -0800

    stack overflow default storage settings test

--- promptme/config/base.py
+++ promptme/config/base.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -152,7 +153,13 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../apps/core/static'),
 )
 
-STATICFILES_STORAGE = 'promptme.storage.WhiteNoiseStaticFilesStorage'
+TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
+
+STATICFILES_STORAGE = (
+    'django.contrib.staticfiles.storage.StaticFilesStorage'
+    if TESTING
+    else 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+)
 
 
 # We also needed to add this to specify where the downloads go

e8cdaf9302c7980d52b1f85284bb2ba7c483ad98
Kevin Dublin <kevin@kevindublin.com>
2019-11-08 15:18:54 -0800

    cleaned up static

--- promptme/config/base.py
+++ promptme/config/base.py
@@ -144,8 +144,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '../apps/core/static'),
+)
 
 # We also needed to add this to specify where the downloads go
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')

c944c80106c835b065b976791a4ef76e27cc1c2b
Kevin Dublin <kevin@kevindublin.com>
2019-11-13 19:08:03 -0800

    trying without manifest

--- promptme/config/base.py
+++ promptme/config/base.py
@@ -152,7 +152,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../apps/core/static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # We also needed to add this to specify where the downloads go
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')

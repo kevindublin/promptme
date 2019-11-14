7822e8222db87b60eafc862f5229e29bff9f320b
Kevin Dublin <kevin@kevindublin.com>
2019-11-13 18:30:47 -0800

    possibly fixes static file heroku issue

--- promptme/config/base.py
+++ promptme/config/base.py
@@ -48,8 +48,8 @@ DJANGO_APPS = [
 INSTALLED_APPS = LOCAL_APPS + THIRD_PARTY_APPS + DJANGO_APPS
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

7ae9d8e6b34e60b3cd3b5b198c923f734474d7e8
Kevin Dublin <kevin@kevindublin.com>
2019-11-29 15:10:58 -0800

    fix migration error

--- apps/accounts/migrations/0006_auto_20191123_0720.py
+++ apps/accounts/migrations/0006_auto_20191123_0720.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AlterField(
             model_name='user',
-            name='level',
+            name='membership',
             field=models.CharField(choices=[('Member', 'Member'), ('Plus', 'Plus'), ('Premium', 'Premium'), ('Professional', 'Professional'), ('Instructor', 'Instructor'), ('Student', 'Student')], default='Member'),
         ),
     ]
